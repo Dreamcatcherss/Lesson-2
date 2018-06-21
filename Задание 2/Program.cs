f215f2da33866a6e06a03b86c898165e2690ed46
Dreamcatcherss <4ydak17@gmail.com>
2018-06-21 12:47:04 +0300

    lesson 2

---
+++ Задание 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = 3.1415292653m;
+            decimal e = 2.7182818284590452m;
+
+            Console.WriteLine(pi);
+            Console.WriteLine(e);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задание 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstVariable = "\nмоя строка 1 ";
+            string secondVariable = "\tмоя строка 2";
+            string thirdVariable = "\aмоя строка 3";
+
+            Console.WriteLine(firstVariable);
+            Console.WriteLine(secondVariable);
+            Console.WriteLine(thirdVariable);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
